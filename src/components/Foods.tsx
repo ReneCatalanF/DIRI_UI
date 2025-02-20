a93e9aaab365e0860efc9daf41168be9e84df182
ReneCatalan <reene.ddyyfmen@gmail.com>
2025-02-20 18:01:28 +0100

    Cards con TailwindUI

--- src/components/Foods.tsx
+++ src/components/Foods.tsx
@@ -1,5 +1,5 @@
 import { MenuItem } from '../entites/entities';
-import './Foods.css'
+//import './Foods.css'
 import FoodOrder from './FoodOrder'
 import ima from '../images/Hamburg.jpg';
 import { useState } from 'react';
@@ -19,7 +19,7 @@ function Foods(props: FoodsProps) {
     };
 
     const handleClick = (menu: MenuItem) => {
-        logger.debug("El usuario quiere ordenar: "+menu.name);
+        logger.debug("El usuario quiere ordenar: " + menu.name);
         setfoodSelect(menu);
         setfoodOrder(!foodOrder);
     };
@@ -29,32 +29,50 @@ function Foods(props: FoodsProps) {
             {!foodOrder && (
                 <>
                     <h4 className="foodTitle">Choose from our Menu</h4>
-                    <ul className="ulFoods">
+                    <br/>
+                    <br/>
+                    <ul className="space-y-10 grid grid-cols-1 md:grid-cols-2 gap-10">
                         {props.foodItems.map((item) => {
                             return (
-                                <li key={item.id} className="liFoods"
-                                    onClick={() =>
-                                        handleClick(item)}>
-                                    <img
-                                        className="foodImg"
-                                        src={ima}
-                                        alt={item.name}
-                                    />
-                                    <div className="foodItem">
-                                        <p className="foodDesc">{item.desc}</p>
-                                        <p data-testid="foodPrice" className="foodPrice">{item.price}$</p>
+                                <>
+                                    <div className="relative flex w-60 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                                        <img className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border 
+                                        text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600"
+                                            src={ima}
+                                            alt={item.name}
+                                        />
+                                        <div className="p-6">
+                                            <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                                                {item.name}
+                                            </h5>
+                                            <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                                                {item.desc}
+                                            </p>
+                                            <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                                                {item.price}$
+                                            </h5>
+                                        </div>
+                                        <div className="p-6 pt-0">
+                                            <button data-ripple-light="true" type="button" className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle 
+                                            font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all 
+                                            hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] 
+                                            active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" onClick={() => handleClick(item)}>
+                                                Pedir
+                                            </button>
+                                        </div>
                                     </div>
-                                </li>
+                                </>
+
                             );
                         })}
                     </ul>
                 </>
             )
             }
-            {foodOrder && 
-            <ErrorBoundary fallback={<div>¡No nos quedan de esas hamburguesas!</div>}><FoodOrder food={foodSelect!} onReturnToMenu={handleReturnToMenu}></FoodOrder></ErrorBoundary>
-            
-            
+            {foodOrder &&
+                <ErrorBoundary fallback={<div>¡No nos quedan de esas hamburguesas!</div>}><FoodOrder food={foodSelect!} onReturnToMenu={handleReturnToMenu}></FoodOrder></ErrorBoundary>
+
+
             }
         </>
     );
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
   // Cargamos variables de entorno según el mode
@@ -11,7 +12,7 @@ export default defineConfig(({ mode }) => {
     build:{
       outDir:'docs',
     },
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
   }
 })
 
